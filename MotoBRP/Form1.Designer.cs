e16545d756cbd264fa3e2e9539d5a4a2ea33d3c5
al-metal <al-metal@bk.ru>
2016-09-09 14:25:15 +0400

    add ecxel.dll

--- MotoBRP/Form1.Designer.cs
+++ MotoBRP/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnActualPrice = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnActualPrice
+            // 
+            this.btnActualPrice.Location = new System.Drawing.Point(12, 12);
+            this.btnActualPrice.Name = "btnActualPrice";
+            this.btnActualPrice.Size = new System.Drawing.Size(143, 23);
+            this.btnActualPrice.TabIndex = 0;
+            this.btnActualPrice.Text = "Обработать прайс";
+            this.btnActualPrice.UseVisualStyleBackColor = true;
+            this.btnActualPrice.Click += new System.EventHandler(this.btnActualPrice_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 434);
+            this.Controls.Add(this.btnActualPrice);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnActualPrice;
     }
 }
 
--- MotoBRP/Form1.cs
+++ MotoBRP/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,18 @@ namespace MotoBRP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnActualPrice_Click(object sender, EventArgs e)
+        {
+            FileInfo file = new FileInfo("Запчасти.xlsx");
+            ExcelPackage p = new ExcelPackage(file);
+            ExcelWorksheet w = p.Workbook.Worksheets[1];
+            int q = w.Dimension.Rows;
+            for (int i = 14; q > i; i++)
+            {
+
+            }
         }
     }
 }
